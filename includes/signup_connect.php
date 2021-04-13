29ceecfec0d2a5cc90f369f98cf18e48246beb8a
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-04-13 15:30:21 +0800

    Update signup_connect.php

--- includes/signup_connect.php
+++ includes/signup_connect.php
@@ -1,25 +1,25 @@
 <?php 
 if (isset($_POST['signupbtn'])) {
 	include_once "conn.php";	
+	include_once "functions.php";	
+
 	$email=htmlentities($_POST['email']);
 	$password=htmlentities($_POST['password']);
 	$confirmpass=htmlentities($_POST['confirmpass']);
 
 	if ($password===$confirmpass) {
-		$sql = "INSERT INTO accounts (email, password)
-			VALUES ( '${email}', '${password}');";
-			
-			if (mysqli_query($conn, $sql)) {
-				session_start();
-				$_SESSION['password']= $password;
-				$_SESSION['email']= $email;
-				header("Location:../index.php");
-				exit();
-			} 
-			else {
-				header("Location:../signup.php?error=1");
-				exit();
-			}		
+		if (createUser($conn, $email, $password)!==false) {
+			session_start();
+			$_SESSION['password']= $password;
+			$_SESSION['email']= $email;
+
+			header("Location:../profile.php");
+			exit();
+
+		}
+		else{
+		header("Location:../signup.php?error=4");
+		exit();
 	}
 	else{
 		header("Location:../signup.php?error=2");
