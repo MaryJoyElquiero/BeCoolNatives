313ad360e0ae06f816ef72aad010fb687490c3e0
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-04-22 13:58:03 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,109 +1,121 @@
-
-<?php 
+<?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>
-		PROFILE
-	</title>
+	<title>ACCOUNT</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="css/profile.css">
+    <link rel="stylesheet" href="css/editprofile.css">
     <link rel="stylesheet" href="font/bootstrap-icons.css">
+	<title>HOME</title>
 </head>
 <body>
-<?php 
 
-if (!isset($_SESSION['password']) || !isset($_SESSION['email'])) {
-	header("Location:signup.php?error=3");
+<?php include "custnav.php";
+if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
+	header("Location:login.php?error=1");
 	exit();
 }
-
- ?>
-
-
-<?php include "custnav.php" ?>
-
-<div class="container-fluid">
-	<div class="profile">
-		<div class="profile-box">
-
-
-			<div class="profile-header">
-				<p class="fs-4">Set Up Profile</p>
-			</div>
-			<?php 
-
-			if (isset($_GET['error'])) {
-		switch ($_GET['error']) {
-			case 1:
-				echo "<p class='text-danger' align='center'>Connection Failed</p>";
-				break;
-			case 2:
-				echo "<p class='text-danger' align='center'>Not Saved</p>";
-				break;
-			
-			default:
-				echo "";
-				break;
-		}
-	}
 ?>
+<div class="container">
+<div class="account">
+	<form action="includes/addprofile.php" method="POST">
 
-			<div class="profile-body">
-				<form action="includes/addprofile.php" method="POST">
-				
-					<div class="field">
-	                    <span class="bi bi-person"></span>
-	                    <input type="text" name="fn" required="" placeholder="Full Name">               
-	                </div> 
-	                <div class="field">
-	                    <span class="bi bi-check"></span>
-	                    <input type="text" name="age" required="" placeholder="Age">               
-	                </div>  
-	                <div class="field">
-	                    <span class="bi bi-check"></span>
-	                    <select class="form-select" name="gender">
-	                    	 <option selected>Gender</option>
-						  <option value="Male">Male</option>
-						  <option value="Female">Female</option> 
-						</select>               
-	                </div> 
-	                <div class="field">
-	                    <span class="bi bi-telephone"></span>
-	                    <input type="text" name="pn" required="" placeholder="Phone Number">               
-	                </div> 
-	          
-	                <div class="address">
-	                	<div class="field">
-	                    	<span class="bi bi-geo-alt"></span>
-	                    	<input type="text" name="province" required="" placeholder="Province">               
-	                	</div>
-	                	<div class="field">
-	                    	<span class="bi bi-check"></span>
-	                    	<input type="text" name="city" required="" placeholder="City">               
-	                	</div> 
-	                	<div class="field">
-	                    	<span class="bi bi-check"></span>
-	                    	<input type="text" name="brgy" required="" placeholder="Barangay">               
-	                	</div>
-	                	<div class="field">
-	                    	<span class="bi bi-check"></span>
-	                    	<input type="text" name="add_details" required="" placeholder="Address Details">               
-	                	</div>
-
-	                </div> 
-	                <button type="submit" class="save" name="save">Save</button>                      
-				</form>
+			<div class="row">
+			<div class="header">
+				<div class="info"> 
+					<div class="text">
+						SET UP PROFILE
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
+		
+		<div class="row">
+			<div class="acc-info">
+				<div class="icon"><i class="bi bi-person"></i></div>
+				<div class="info"> 
+					<div class="label">Full Name:</div>
+					<div class="text">
+						<input type="text" name="cn" placeholder="Full Name" autofocus>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="add-info">
+				<div class="icon"><i class="bi bi-geo"></i></div>
+				<div class="info"> 
+					<div class="label">Address:</div>
+					<div class="address">
+
+						<input type="text" name="province" placeholder="Province">
+						
+						<input type="text" name="city"  placeholder="City">
+						
+						<input type="text" name="brgy" Placeholder="Baranggay">
+						
+						<input type="text" name="add_details" placeholder="Address Details">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="acc-info">
+				<div class="icon"><i class="bi bi-telephone"></i></div>
+				<div class="info"> 
+					<div class="label">Phone Number:</div>
+					<div class="text">
+						<input type="text" name="pn" placeholder="Phone Number">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="acc-info">
+				<div class="icon"><i class="bi bi-person-check"></i></div>
+				<div class="info"> 
+					<div class="label">Age:</div>
+					<div class="text">
+						<input type="number" name="age" placeholder="Age">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="acc-info">
+				<div class="icon"><i class="bi bi-person-check""></i></div>
+				<div class="info"> 
+					<div class="label">Gender:</div>
+					<div class="text">
+						 <select class="form-select" name="gender" required="">
+	                      <option selected>Select</option>
+						  <option value="Male">Male</option>
+						  <option value="Female">Female</option> 
+						</select>    
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="savebtn">
+				<div class="info"> 
+					<button type="submit" name="save"> Save </button>
+				</div>
+			</div>
+		</div>
+		
+</form>
+</div>
 </div>
 
 
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/bootstrap.bundle.min.js"></script>
-	<script src="js/custom.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/custom.js"></script>
+
 </body>
 </html>
