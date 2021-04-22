0f2badb7341bd428b446a3cd07bd2e5e46d15312
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-04-22 14:18:07 +0800

    Update placeorder.php

--- includes/placeorder.php
+++ includes/placeorder.php
@@ -7,18 +7,28 @@ if (isset($_POST['placeorder'])) {
 		$item_price=htmlentities($_POST['item_price']);
 		$order_qty=htmlentities($_POST['order_qty']);
 		$order_total=htmlentities($_POST['order_total']);
+		$billing_info=htmlentities($_POST['billing_info']);
 		$order_date=date("Y-m-d");
 		$order_status="To Pay";
 
-	$buy="INSERT INTO orders(acc_id, item_id, item_price, order_qty, order_total, order_date, order_status) VALUES ('$acc_id','$item_id','$item_price', '$order_qty','$order_total','$order_date','$order_status')";
+	$buy="INSERT INTO orders(acc_id, item_id, item_price, order_qty, order_total, order_date, order_status, billing_info) VALUES ('$acc_id','$item_id','$item_price', '$order_qty','$order_total','$order_date','$order_status', '$billing_info')";
 
-		if (mysqli_query($conn, $buy)) {	
-					header("Location:../myorders.php");
-					exit();
+		if (mysqli_query($conn, $buy)) {
+
+					$delete="DELETE FROM cart WHERE item_id= '$item_id' AND acc_id ='$acc_id';";
+
+					if (mysqli_query($conn, $delete)) {	
+					header("Location:../topay.php");
+						exit();
 					} 
 					else {
 						header("Location:../cart.php?error=1");
 						exit();
+					}	
+			} 
+		else {
+			header("Location:../cart.php?error=1");
+			exit();
 					}		
 
 }
