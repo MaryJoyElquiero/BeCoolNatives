fa916103e52cad49acd0b3bbf0c28f170159702f
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-06 15:46:18 +0800

    Add files via upload

---
+++ includes/addshop.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+if (isset($_POST['save'])) {
+	include_once "conn.php";
+
+	$shop_name=htmlentities($_POST['shop_name']);
+
+	$sql="SELECT acc_id FROM accounts WHERE email='{$_SESSION['email']}' 
+	and password='{$_SESSION['password']}';";
+	$result = mysqli_query($conn, $sql); 
+	if (mysqli_num_rows($result)) { 
+	    while ($row = mysqli_fetch_assoc($result)) { 
+	        $acc_id=$row['acc_id'];
+	    }
+
+	$sql2="INSERT INTO shop(shop_name, acc_id) VALUES (?,?);";
+	$stmt= mysqli_stmt_init($conn);
+
+	if (!mysqli_stmt_prepare($stmt,$sql2)) {
+		header("Location:../shopindex.php?error");
+		exit();
+	}
+	mysqli_stmt_bind_param($stmt, "ss", $shop_name, $acc_id);
+	mysqli_stmt_execute($stmt);
+
+	header("Location:../shopindex.php");
+	exit();
+}
+
+}
+ ?>
