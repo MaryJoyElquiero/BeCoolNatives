98055d1f0e660d64bbb61b75f8554fa7a1b97294
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-11 17:25:49 +0800

    Update deleteItem.php

--- includes/deleteItem.php
+++ includes/deleteItem.php
@@ -1,7 +1,29 @@
 <?php 
 
 
-if (isset($_POST['delete'])) {
+if (isset($_POST['delete1'])) {
+
+include_once "conn.php";
+
+	$item_id=htmlentities($_POST['item_id']);
+
+	$sql = "DELETE FROM items WHERE item_id ='$item_id';";
+
+		if (mysqli_query($conn, $sql)) {	
+					header("Location:../shopindex.php?error=3");
+						exit();
+					} 
+					else {
+						header("Location:../shopindex.php?error=2");
+						exit();
+					}		
+
+
+
+
+}
+
+if (isset($_POST['delete2'])) {
 
 include_once "conn.php";
 
@@ -15,6 +37,28 @@ include_once "conn.php";
 					} 
 					else {
 						header("Location:../products.php?error=5");
+						exit();
+					}		
+
+
+
+
+}
+
+if (isset($_POST['delete3'])) {
+
+include_once "conn.php";
+
+	$item_id=htmlentities($_POST['item_id']);
+
+	$sql = "DELETE FROM items WHERE item_id ='$item_id';";
+
+		if (mysqli_query($conn, $sql)) {	
+					header("Location:../admin_products.php?error=6");
+						exit();
+					} 
+					else {
+						header("Location:../admin_products.php?error=5");
 						exit();
 					}		
 
