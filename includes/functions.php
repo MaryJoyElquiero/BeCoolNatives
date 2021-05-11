d38a16baf556c13e5325867db29cc580a4872228
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-11 17:26:57 +0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -43,5 +43,29 @@ function uidExists($conn, $email, $password){
 		}
 
 
+function adminlogin($conn, $email, $password){
+		$sql="SELECT * FROM admin WHERE admin_email=? AND admin_pass=?;";
+		$stmt= mysqli_stmt_init($conn);
+		if (!mysqli_stmt_prepare($stmt, $sql)) {
+				header("Location:../admin_login.php?error=3");
+				exit();
+			}
+			mysqli_stmt_bind_param($stmt,"ss", $email, $password);
+			mysqli_stmt_execute($stmt);
+			$result= mysqli_stmt_get_result($stmt);
+
+			if($row=mysqli_fetch_assoc($result)) {	
+				return $row;
+			}
+			else{
+				$err= false;
+				return $err;
+			}
+			mysqli_stmt_close($stmt);
+
+				
+		}
+
+
 
 ?>
