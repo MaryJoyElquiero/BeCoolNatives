3da9e759dfcef603f57ae1390c4efe9c206ce009
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-11 17:27:29 +0800

    Update login_connect.php

--- includes/login_connect.php
+++ includes/login_connect.php
@@ -13,15 +13,41 @@ if(isset($_POST['submit'])){
 		$_SESSION['password']= $password;
 		$_SESSION['email']= $email;
 
-
 		header("Location:../index.php?error=4");
 		exit();
+	
 	}
 	else{
 		header("Location:../login.php?error=2");
 		exit();
 	}
-}
 
+
+}
+if (isset($_POST['adminlgn'])) {
+		include_once "conn.php";
+		include_once "functions.php";
+
+		
+	$email=htmlentities($_POST['email']);
+	$password=htmlentities($_POST['password']);
+
+
+	if (adminlogin($conn, $email, $password)!==false) {
+		session_start();
+		$_SESSION['admin_pass']= $password;
+		$_SESSION['admin_email']= $email;
+
+		header("Location:../admin_dashboard.php");
+		exit();
+	
+	}
+	else{
+		header("Location:../admin_login.php?error=2");
+		exit();
+	}
+
+
+	}
 
  ?>
