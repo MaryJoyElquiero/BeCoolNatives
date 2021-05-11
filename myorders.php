5f9b694330f49f4eeb03000b9df28a65314f1c2a
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-11 16:58:33 +0800

    Update myorders.php

--- myorders.php
+++ myorders.php
@@ -15,6 +15,23 @@ session_start();
 if (!isset($_SESSION['password']) || !isset($_SESSION['email'])) {
 	header("Location:login.php?error=1");
 	exit();
+}<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="css/myorders.css">
+    <link rel="stylesheet" href="font/bootstrap-icons.css">
+	<title>MYORDERS</title>
+</head>
+<body>
+<?php 
+
+if (!isset($_SESSION['password']) || !isset($_SESSION['email'])) {
+	header("Location:login.php?error=1");
+	exit();
 }
 
  ?>
