5f8902f9a6cb56465959a5bd65995b43c540f892
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-11 17:30:07 +0800

    Update updateadd.php

--- includes/updateadd.php
+++ includes/updateadd.php
@@ -9,6 +9,10 @@ if(isset($_POST['save'])){
 	$add_details=htmlentities($_POST['add_details']);
 	$acc_id=htmlentities($_POST['acc_id']);
 	$item_id=htmlentities($_POST['item_id']);	
+	$id=htmlentities($_POST['id']);	
+	$item_qty=htmlentities($_POST['item_qty']);	
+	$price_amt=htmlentities($_POST['price_amt']);	
+	$total_amt=htmlentities($_POST['total_amt']);	
 	
 
 	$sql="UPDATE accinfo 
@@ -20,12 +24,17 @@ if(isset($_POST['save'])){
 		WHERE acc_id ='$acc_id';";
 
 	if (mysqli_query($conn,$sql)){
-		if (!isset($_POST['item_id'])) {
-			header("Location:../account.php?Saved");
+		if (isset($_POST['id'])) {
+			header("Location:../orderform.php?id=$id");
+			exit();
+		}
+		elseif(isset($_POST['item_id'])&&isset($_POST['item_qty'])&&isset($_POST['price_amt'])&&isset($_POST['total_amt'])) {
+			header("Location:../orderform2.php?item_id=$item_id&&item_qty=$item_qty&&total_amt=$total_amt&&price_amt=$price_amt");
 			exit();
 		}
 		else{
-			header("Location:../orderform.php?id=$item_id");
+			
+			header("Location:../account.php?Saved");
 			exit();
 		}
 	}
