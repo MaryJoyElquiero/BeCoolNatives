46fde68d97712a438067f61c8aa173a376d4672c
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-15 13:13:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ session_start();
 		<?php 
 		include "includes/conn.php";
 
-		$sql= "SELECT * FROM category WHERE cat_status ='A';";
+		$sql= "SELECT * FROM category WHERE cat_status ='Active';";
 
 		$stmt= mysqli_stmt_init($conn);
 		if(!mysqli_stmt_prepare($stmt,$sql)) {
