1f3ba3e5ce7c784e792805fe44a0825a764f1599
MaryJoyElquiero <82370550+MaryJoyElquiero@users.noreply.github.com>
2021-05-18 10:15:19 +0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -66,6 +66,27 @@ function adminlogin($conn, $email, $password){
 				
 		}
 
+function emailExists($conn, $email){
+		$sql="SELECT * FROM accounts WHERE email=?;";
+		$stmt= mysqli_stmt_init($conn);
+		if (!mysqli_stmt_prepare($stmt, $sql)) {
+				header("Location:../login.php?error=3");
+				exit();
+			}
+			mysqli_stmt_bind_param($stmt,"s", $email);
+			mysqli_stmt_execute($stmt);
+			$result= mysqli_stmt_get_result($stmt);
 
+			if($row=mysqli_fetch_assoc($result)) {	
+				return $row;
+			}
+			else{
+				$err= false;
+				return $err;
+			}
+			mysqli_stmt_close($stmt);
+
+				
+		}
 
 ?>
